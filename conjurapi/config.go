092b57fcc2fd691e170a0dea6e391e0896b4046c
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2017-08-15 00:11:29 +0100

    feat: add LoadConfigFromEnv

--- conjurapi/config.go
+++ conjurapi/config.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"os"
 )
 
 type Config struct {
-	Account        string `validate:"required"`
-	APIKey         string
-	ApplianceURL   string `validate:"required"`
-	Login          string
-	AuthnTokenFile string
+	Account        string `validate:"required" env:"CONJUR_ACCOUNT"`
+	APIKey         string `env:"CONJUR_AUTHN_API_KEY"`
+	ApplianceURL   string `validate:"required" env:"CONJUR_APPLIANCE_URL"`
+	Login          string `env:"CONJUR_AUTHN_LOGIN"`
+	AuthnTokenFile string `env:"CONJUR_AUTHN_TOKEN_FILE"`
 }
 
-const tagName = "validate"
-
-func (c Config) validate() (error) {
-	v := reflect.ValueOf(c)
+func (c *Config) validate() (error) {
+	v := reflect.ValueOf(*c)
 	errors := []string{}
 
+	const tagName = "validate"
 	for i := 0; i < v.NumField(); i++ {
 		f := v.Type().Field(i)
 		tag := f.Tag.Get(tagName)
@@ -40,3 +40,25 @@ func (c Config) validate() (error) {
 	return fmt.Errorf("%s", strings.Join(errors, "\n"))
 }
 
+func LoadConfigFromEnv() Config {
+	const tagName = "env"
+	c := Config{}
+
+	vElem := reflect.ValueOf(&c).Elem()
+	vType := reflect.ValueOf(c).Type()
+
+	for i := 0; i < vElem.NumField(); i++ {
+		typeField := vType.Field(i)
+		elemField := vElem.Field(i)
+		tag := typeField.Tag.Get(tagName)
+
+		switch elemField.Interface().(type) {
+		case string:
+			elemField.SetString(os.Getenv(tag))
+		default:
+			continue
+		}
+
+	}
+	return c
+}
--- conjurapi/config_test.go
+++ conjurapi/config_test.go
@@ -3,6 +3,7 @@ package conjurapi
 import (
 	"testing"
 	. "github.com/smartystreets/goconvey/convey"
+	"os"
 )
 
 func TestConfig_IsValid(t *testing.T) {
@@ -28,5 +29,30 @@ func TestConfig_IsValid(t *testing.T) {
 		errString := err.Error()
 
 		So(errString, ShouldContainSubstring, "ApplianceURL is required.")
+	})
+}
+
+func TestLoadConfigFromEnv(t *testing.T) {
+	Convey("Given configuration and authentication credentials in env", t, func() {
+		e := ClearEnv()
+		defer e.RestoreEnv()
+
+		os.Setenv("CONJUR_ACCOUNT", "account")
+		os.Setenv("CONJUR_AUTHN_API_KEY", "authn-api-key")
+		os.Setenv("CONJUR_APPLIANCE_URL", "appliance-url")
+		os.Setenv("CONJUR_AUTHN_LOGIN", "authn-login")
+		os.Setenv("CONJUR_AUTHN_TOKEN_FILE", "authn-token-file")
+
+		Convey("Returns Config loaded with values from env", func() {
+			config := LoadConfigFromEnv()
+
+			So(config, ShouldResemble, Config{
+				Account: "account",
+				APIKey: "authn-api-key",
+				ApplianceURL: "appliance-url",
+				Login: "authn-login",
+				AuthnTokenFile: "authn-token-file",
+			})
+		})
 	})
 }
