8c26754b598fb8a2747eb168d3645f02091ef280
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2017-09-05 00:21:21 -0400

    fix test.sh

--- test.sh
+++ test.sh
@@ -35,4 +35,4 @@ docker-compose exec -T test env \
     CONJUR_AUTHN_API_KEY="$api_key" \
     CONJUR_V4_AUTHN_API_KEY="$api_key_v4" \
     CONJUR_V4_SSL_CERTIFICATE="$ssl_cert_v4" \
-    bash -c 'go test -v "$(go list ./... | grep -v /vendor/)" | tee output/junit.output && cat output/junit.output | go-junit-report > output/junit.xml'
+    bash -c 'go test "$(go list ./... | grep -v /vendor/)" | tee output/junit.output && cat output/junit.output | go-junit-report > output/junit.xml'
