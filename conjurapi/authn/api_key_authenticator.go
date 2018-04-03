4aa9ad0923dff6ecce69f2aea0b1e0aa3c8f7f5a
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2018-04-03 12:50:29 +0100

    fix test condition

--- conjurapi/authn/api_key_authenticator.go
+++ conjurapi/authn/api_key_authenticator.go
@@ -2,6 +2,8 @@ package authn
 
 import "time"
 
+// The four minutes is to work around a bug in Conjur < 4.7 causing a 404 on
+// long-running operations (when the token is used right around the 5 minute mark).
 var TOKEN_STALE = 4 * time.Minute
 
 type APIKeyAuthenticator struct {
@@ -24,5 +26,5 @@ func (a *APIKeyAuthenticator) RefreshToken() ([]byte, error) {
 }
 
 func (a *APIKeyAuthenticator) NeedsTokenRefresh() bool {
-	return time.Now().Sub(a.tokenBorn) > TOKEN_STALE
+	return time.Now().Sub(a.tokenBorn) >= TOKEN_STALE
 }
