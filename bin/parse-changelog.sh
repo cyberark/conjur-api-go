b6e2f988f190b7e75232b17f631cf9dbbadc3c96
junior.taeza <junior.taeza@cyberark.com>
2023-04-25 11:19:26 -0400

    Upgrade ruby from 2.5 to 3.1 in bin/parse-changelog.sh

--- bin/parse-changelog.sh
+++ bin/parse-changelog.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 docker run --rm \
   -v "$(pwd)/..:/work" \
   -w "/work" \
-  ruby:2.5 bash -ec "
+  ruby:3.1 bash -ec "
     gem install -N parse_a_changelog
     parse ./CHANGELOG.md
   "
